1ecbf2e87a553b83b0e87b245bbfee714bc16992
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-01-04 01:21:25 +0530

    Added js-lexer project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,6 +10,7 @@ import palettePixImg from '../assets/palettepix.png';
 import blogImg from "../assets/blog.png"
 import interpreterImg from "../assets/interpreter.png"
 import frmtImg from "../assets/frmt.png"
+import jsLexerImg from "../assets/js-lexer.png"
 
 const Projects = () => {
 
@@ -19,6 +20,7 @@ const Projects = () => {
         setProjects([{name: 'Palette Pix', description: 'A tool where designers and developers can quickly test out color palettes. Users can change to palettes of their choice or get a random palette.', img: palettePixImg, website: 'https://shobhit-nagpal.github.io/palettepix/', repo: 'https://github.com/Shobhit-Nagpal/palettepix'}, 
                      {name: 'Monkey interpreter', description: 'An interpreter for Monkey language. Built by reading Writing an Interpreter in Go.', img: interpreterImg, website: '', repo: 'https://github.com/Shobhit-Nagpal/monkey-interpreter'},
                      {name: 'Frmt', description: 'Frmt is a CLI tool written in Golang to convert file formats from your CLI.', img: frmtImg, website: '', repo: 'https://github.com/Shobhit-Nagpal/frmt'},
+                     {name: 'JS-Lexer', description: 'JS-Lexer is a project inspired from making a Monkey interpreter in Go by reading "Writing an Interpreter in Go" by Thorsten Ball. The goal was simply to build the lexer out on my own in JS. Published as an npm package.', img: jsLexerImg, website: 'https://www.npmjs.com/package/@shobhit-nagpal/js-lexer', repo: 'https://github.com/Shobhit-Nagpal/js-lexer'},
                      {name: 'Personal Blog', description: 'A blog for personal thoughts, made using MERN stack. Blogs inspired from Inner Monologue.', img: blogImg, website: '', repo: 'https://github.com/Shobhit-Nagpal/blog'},
                      {name: 'Youtube Clone', description: 'A Youtube clone using React, Rapid API, Firebase where a user can see the home feed, search for videos/channels and watch videos.', img: youtubeCloneImg, website: 'https://youtube-clone-dusky-nine.vercel.app/', repo: 'https://github.com/Shobhit-Nagpal/youtube-clone'},
                      {name: 'Tag', description: 'A photo tagging game where you have to find your favourite game characters. Made using React, React Router and Firebase', img: tagImg, website: 'https://shobhit-nagpal.github.io/tag/', repo: 'https://github.com/Shobhit-Nagpal/tag'} , 
